2de751be119b5c8eebb9666d8e819bfcf0caf2c1
Raghuram Kokalla <raghuram@sg.ibm.com>
2021-11-19 18:53:30 +0800

    ci: git changes log

--- .github/scripts/pushdocs.sh
+++ .github/scripts/pushdocs.sh
@@ -20,13 +20,16 @@ mkdir -p "${DOC_TARGET_FOLDER}"
 cd "${SOURCE_ROOT}/packages/${MODULE}" && npm i && npm run docs && cd -
 cp -R "${SOURCE_ROOT}/packages/${MODULE}/docs/." "${DOC_TARGET_FOLDER}/"
 
+ls -al ${GITHUB_WORKSPACE}
 ls -al ${DESTINATION_ROOT}
 ls -al ${DESTINATION_ROOT}/docs
 
 # configure and push to github docs repo
 cd ${SOURCE_ROOT}
 MESSAGE=`git log --format=%B -n 1`
+echo "msg=${MESSAGE}"
 cd ${DESTINATION_ROOT}
+ls -al .
 git config -l | grep 'http\..*\.extraheader' | cut -d= -f1 | xargs -L1 git config --unset-all
 git config user.email "raghu4u449@gmail.com"
 git config user.name "rramk"
