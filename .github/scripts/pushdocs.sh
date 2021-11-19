8466f4905fee71c2f41cf3f26918fa713cf657e3
Raghuram Kokalla <raghuram@sg.ibm.com>
2021-11-19 19:16:58 +0800

    ci: git changes log 6

--- .github/scripts/pushdocs.sh
+++ .github/scripts/pushdocs.sh
@@ -17,6 +17,9 @@ cp -R ${SOURCE_ROOT}/packages/${MODULE}/docs/. ${DOC_TARGET_FOLDER}/
 cd ${SOURCE_ROOT}
 MESSAGE=`git log --format=%B -n 1`
 cd ${DESTINATION_ROOT}
+# thanks to https://stackoverflow.com/a/64271581/5099773
+git config -l | grep 'http\..*\.extraheader' | cut -d= -f1 | \
+    xargs -L1 git config --unset-all
 git config user.email "raghu4u449@gmail.com"
 git config user.name "rramk"
 git add -A
