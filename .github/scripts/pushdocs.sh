0af8ed6f01247f7df861067b5ba7f170dca99da6
Raghuram Kokalla <raghuram@sg.ibm.com>
2021-11-19 13:44:39 +0800

    debug: log

--- .github/scripts/pushdocs.sh
+++ .github/scripts/pushdocs.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-env
 pwd
 ls ${GITHUB_WORKSPACE}
 cd ${GITHUB_WORKSPACE}/monorepo/packages/foo && npm i && npm run docs && cd -
@@ -16,7 +15,17 @@ git checkout ${PAGES_BRANCH} || git checkout -b ${PAGES_BRANCH}
 DOC_TARGET_FOLDER="docs"
 rm -rf ${DOC_TARGET_FOLDER}/*
 mkdir -p ${DOC_TARGET_FOLDER}
-"\\cp -R ${GITHUB_WORKSPACE}/monorepo/packages/foo/docs/. ${DOC_TARGET_FOLDER}/"
+
+pwd
+ls -al
+ls -al ../
+ls -al ../../
+
+cp -R ${GITHUB_WORKSPACE}/monorepo/packages/foo/docs/. ${DOC_TARGET_FOLDER}/
+
+git config user.email "raghu4u449@gmail.com"
+git config user.name "rramk"
+
 git add -A
 git diff-index --quiet HEAD || git commit -m "${MESSAGE}"
 git push -u https://${GH_PAGES_TOKEN}@github.com/rramk/rramtravisghtest
