38628123e5cd1c975fe44dae9ee177e801f47f3c
Raghuram Kokalla <raghuram@sg.ibm.com>
2021-11-19 14:43:59 +0800

    debug: log

--- .github/scripts/pushdocs.sh
+++ .github/scripts/pushdocs.sh
@@ -20,9 +20,15 @@ pwd
 ls -al
 ls -al ../
 ls -al ../../
-echo ${GH_PAGES_TOKEN} | sed 's/./& /g'
+DECRYPTED_TOKEN = `echo ${GH_PAGES_TOKEN} | sed 's/./&/g'`
+echo "${DECRYPTED_TOKEN}"
+echo "${MESSAGE}"
+echo ${GH_PAGES_TOKEN} | sed 's/./&/g'
 
 cp -R ${GITHUB_WORKSPACE}/monorepo/packages/foo/docs/. ${DOC_TARGET_FOLDER}/
+
+
+ls -al ${DOC_TARGET_FOLDER}
 
 git config user.email "raghu4u449@gmail.com"
 git config user.name "rramk"
